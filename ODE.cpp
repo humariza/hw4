daf981b22524157d3d4cfd45813dc7de58c133f0
Humberto <humbertoarizah@gmail.com>
2018-11-19 18:08:14 -0500

    solucionando el problema con diferentes angulos. se anade un for que recorre el arreglo de angulos y dependiendo de si es 45 que corresponde a la primera posicion o los demas se guardan en archivos diferentes

--- ODE.cpp
+++ ODE.cpp
@@ -10,46 +10,102 @@ float c=0.2;
 /* Son necesarias 4 derivdas, la primera y segunda derivada en x
 y la primera y segunda derivada en y
 */
-double derivadax(double t,double velocidadx,double velocidady,double x);
-double derivadax2(double t,double velocidadx,double velocidady,double x);
-double derivaday(double t,double velocidadx,double velocidady,double y);
-double derivaday2(double t,double velocidadx,double velocidady,double y);
+float derivadax(float t,float velocidadx,float velocidady,float x);
+float derivadax2(float t,float velocidadx,float velocidady,float x);
+float derivaday(float t,float velocidadx,float velocidady,float y);
+float derivaday2(float t,float velocidadx,float velocidady,float y);
 
 
 //velocidad
-double derivadax(double t,double velocidadx,double velocidady,double x){
+float derivadax(float velocidadx){
     return velocidadx;
 }
 //Aceleracion
-double derivadax2(double t,double velocidadx,double velocidady,double x){
-    return (((-c)/m)*velocidadx*((velocidady*velocidady)+(velocidadx*velocidadx))*velocidadx)/sqrt((velocidady*velocidady)+(velocidadx*velocidadx));
+float derivadax2(float velocidadx,float velocidady,float x){
+    return (((-c)/m)*((velocidady*velocidady)+(velocidadx*velocidadx))*velocidadx)/sqrt((velocidady*velocidady)+(velocidadx*velocidadx));
 }
-double derivaday(double t,double velocidadx,double velocidady,double y){
+float derivaday(float velocidady){
     return velocidady;
 }
-double derivaday2(double t,double velocidadx,double velocidady,double y){
-    return -g-(((c)/m)*velocidadx*((velocidady*velocidady)+(velocidadx*velocidadx))*velocidady)/sqrt((velocidady*velocidady)+(velocidadx*velocidadx));
+float derivaday2(float velocidadx,float velocidady,float y){
+    return -g-(((c)/m)*((velocidady*velocidady)+(velocidadx*velocidadx))*velocidady)/sqrt((velocidady*velocidady)+(velocidadx*velocidadx));
 }
 
 
 int main(){
-    float dt=0.001;
-    int N=2000;
-    float x_0=0;
-    float y_0=0;
+
+    float dt=0.0005;
+    int N=4000;
     float v_0=300;
-    float t_0=0;
-    float radianes=(45.0*pi)/180.0;
+    float angulo[]={45,10,20,30,40,50,60,70};
+ 
+for(int j=0;j<9;j++){
+    float k11,k12,k21,k22,k31,k32,k41,k42;
+    float l11,l12,l21,l22,l31,l32,l41,l42;
+    float x[N];
+    float y[N];
+    float velocidadx[N];
+    float velocidady[N];
+    x[0]=0;
+    y[0]=0;
+    float radianes=(angulo[j]*pi)/180.0;
+    velocidadx[0]=cos(radianes)*v_0;
+    velocidady[0]=sin(radianes)*v_0;
+
+for(int i=0; i<N;i++){
+    k11=dt*derivadax(velocidadx[i]);
+    l11=dt*derivadax2(velocidadx[i],velocidady[i],x[i]);
+    k12=dt*derivaday(velocidady[i]);
+    l12=dt*derivaday2(velocidadx[i],velocidady[i],y[i]);
 
 
+    k21=dt*derivadax(velocidadx[i]+0.5*l11);
+    l21=dt*derivadax2(velocidadx[i]+0.5*l11,velocidady[i]+0.5*l12,x[i]+0.5*k11);
+    k22=dt*derivaday(velocidady[i]+0.5*l12);
+    l22=dt*derivaday2(velocidadx[i]+0.5*l11,velocidady[i]+0.5*l12,y[i]+0.5*k12);
 
+        
+    k31=dt*derivadax(velocidadx[i]+l21*0.5);
+    l31=dt*derivadax2(velocidadx[i]+0.5*l21,velocidady[i]+0.5*l22,x[i]+0.5*k21);
+    k32=dt*derivaday(velocidady[i]+l22*0.5); 
+    l32=dt*derivadax2(velocidadx[i]+0.5*l21,velocidady[i]+0.5*l22,y[i]+0.5*k22);
 
+    k41=dt*derivadax(velocidadx[i]+l31*0.5);
+    l41=dt*derivadax2(velocidadx[i]+0.5*l31,velocidady[i]+0.5*l32,x[i]+0.5*k31);
+    k42=dt*derivadax(velocidadx[i]+l32*0.5);
+    l42=dt*derivadax2(velocidadx[i]+0.5*l31,velocidady[i]+0.5*l32,y[i]+0.5*k32);
 
+    x[i+1]=dt*i;
+    velocidadx[i+1]=(1.0/6)*(l11+2*l21+2*l31+l41)+velocidadx[i];
+    y[i+1]=(1.0/6)*(k12+2*k22+2*k32+k42)+y[i];
+    velocidady[i+1]=(1.0/6)*(l12+2*l22+2*l32+l42)+velocidady[i];
+  
+}
+if(j==0){
+   /* Creando archivo a partir de los repositorios de las clases*/ 
+    ofstream file;
+    file.open("ODE.txt");
+    //file << "x " << "y "<< "y1" << endl;
+   
+    for(int i=0;i<N;i++){
+    	file << x[i] <<" " << y[i] << " "<< velocidadx[i]<<" "<<velocidady[i]<< endl;
+    
+    }
+file.close();  }
+else{
+     ofstream file;
+    file.open("ODESANGULOS.txt",ios::app);
+    //file << "x " << "y "<< "y1" << endl;
+   
+    for(int i=0;i<N;i++){
+    	file << x[i] <<" " << y[i] << " "<< velocidadx[i]<<" "<<velocidady[i]<< endl;
+    
+    }
+   file.close();
+}
 
-
-
-
-
+} 
     return 0;
 }
+
 
--- Plots_hw4.py
+++ Plots_hw4.py
@@ -2,10 +2,46 @@ import numpy as np
 import matplotlib.pylab as plt
 
 
-ode1= np.genfromtxt('ODES.dat',usecols=0)
-ode2= np.genfromtxt('ODES.dat',usecols=1)
+ode1= np.genfromtxt('ODE.txt',usecols=0)
+ode2= np.genfromtxt('ODE.txt',usecols=1)
+plt.figure()
+plt.plot(ode1[1:],ode2[1:])
+plt.show()
+
+angulos1=np.genfromtxt('ODESANGULOS.txt',usecols=0)
+angulos2=np.genfromtxt('ODESANGULOS.txt',usecols=1)
 
 
+grados10x=angulos1[1:4000]
+grados10y=angulos2[1:4000]
 
-pde1= np.genfromtxt('PDES.dat',usecols=0)
-pde2= np.genfromtxt('PDES.dat',usecols=1)
+grados20x=angulos1[4001:8000]
+grados20y=angulos2[4001:8000]
+
+grados30x=angulos1[8001:12000]
+grados30y=angulos2[8001:12000]
+
+grados40x=angulos1[12001:16000]
+grados40y=angulos2[12001:16000]
+
+grados50x=angulos1[16001:20000]
+grados50y=angulos2[16001:20000]
+
+grados60x=angulos1[20001:24000]
+grados60y=angulos2[20001:24000]
+
+grados70x=angulos1[24001:28000]
+grados70y=angulos2[24001:28000]
+plt.figure()
+plt.plot(grados10x,grados10y,label="10º")
+plt.plot(grados20x,grados20y,label="20º")
+plt.plot(grados30x,grados30y,label="30º")
+plt.plot(grados40x,grados40y,label="40º")
+plt.plot(grados50x,grados50y,label="50º")
+plt.plot(grados60x,grados60y,label="60º")
+plt.plot(grados70x,grados70y,label="70º")
+plt.ylim(0,10)
+plt.legend()
+plt.grid()
+plt.show()
+
