d6bad9526e55ca32ab6b9d294ad5fea3eac872d2
Humberto <humbertoarizah@gmail.com>
2018-11-19 19:00:08 -0500

    se crean las condiciones iniciales

--- PDE.cpp
+++ PDE.cpp
@@ -2,9 +2,19 @@
 #include <fstream>
 #include <math.h>
 #include <stdio.h>
+/* 
+Condiciones iniciales
+*/
+float k=1.62;
+float cp=820;
+float rho=2.71;
+float radio=5;
+float v;
+float tvarilla=100;
+float tcalcita=10;
 
-
-int main{
+int main(){
+v=k/(cp*rho);
 
 
     return 0;
